38c5b6188ba99f0deb2f56a61ce55de9254721df
Artem Smotrakov <artem.smotrakov@gmail.com>
2016-09-29 22:27:33 -0700

    Parse command line options

--- tcpooh.py
+++ tcpooh.py
@@ -2,27 +2,64 @@
 
 import socket
 
+# TODO: UDP support
+
 parser = argparse.ArgumentParser()
-parser.add_argument('--local_host', help='local host name', default='localhost')
-parser.add_argument('--local_port', help='local port to listen', type=int, default=10101)
+parser.add_argument('--local_host',  help='local host name', default='localhost')
+parser.add_argument('--local_port',  help='local port to listen', type=int, default=10101, required=True)
 parser.add_argument('--remote_host', help='remote host name', default='localhost')
-parser.add_argument('--remote_port', help='remote port', type=int, default=80)
+parser.add_argument('--remote_port', help='remote port', type=int, default=80, required=True)
 parser.add_argument('--test',
                     help='test range, it can be a number, or an interval "start:end"')
 parser.add_argument('--ratio',
                     help='fuzzing ratio range, it can be a number, or an interval "start:end"',
                     default='0.01:0.05')
+parser.add_argument('--protocol', help='TCP or UDP', choices=['tcp', 'udp'], default='tcp')
+
 args = parser.parse_args()
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((args.local_host, args.port))
-    s.listen(1)
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(4096)
-            raise Exception('Not implemented')
+if args.test:
+    parts = args.test.split(':')
+    if len(parts) == 1:
+        start_test = int(parts[0])
+        end_test = start_test
+    elif len(parts) == 2:
+        start_test = int(parts[0])
+        if parts[1] == '' or parts[1] == 'infinite':
+            end_test = float('inf')
+        else:
+            end_test = int(parts[1])
+    else:
+        raise Exception('Could not parse --test value, too many colons')
+else:
+    start_test = 0
+    end_test = float('inf')
+
+parts = args.ratio.split(':')
+if len(parts) == 1:
+    min_ratio = float(parts[0])
+    max_ratio = min_ratio
+elif len(parts) == 2:
+    min_ratio = float(parts[0])
+    max_ratio = float(parts[1])
+else:
+    raise Exception('Could not parse --ratio value, too many colons')
+
+if args.protocol == 'tcp':
+    run_tcp_server(args.local_host, args.local_port, args.remote_host, args.remote_port)
+elif args.protocol == 'udp':
+    raise Exception('UDP is not supported yet')
+
+def run_tcp_server(local_host, local_port, remote_host, remote_port):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((local_host, local_port))
+        s.listen(1)
+        conn, addr = s.accept()
+        with conn:
+            print('Connected by', addr)
+            while True:
+                data = conn.recv(4096)
+                raise Exception('Not implemented')
 
 def fuzz(data):
     # do nothing so far
