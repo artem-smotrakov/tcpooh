1b5d2dc110312921dfb4dc47bc25c937d5e2c0f1
Artem Smotrakov <artem.smotrakov@gmail.com>
2016-11-06 21:13:42 -0800

    Added Server class

--- tcpooh.py
+++ tcpooh.py
@@ -9,73 +9,78 @@ import helper
 # TODO: add Config class
 # TODO: implement fuzzing (two directions)
 
-# global variables
-bufsize = 4096
+# This is a simple TCP/TLS server which just wraps socket's methods
+class Server:
 
-def run_tcp_server(local_host, local_port, remote_host, remote_port, timeout):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
-        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        server.bind((local_host, local_port))
-        server.listen(1)
-        helper.print_with_prefix('server', 'listening on {0:s}:{1:d}'.format(local_host, local_port))
-        while True:
-            helper.print_with_prefix('server', 'waiting for connection')
-            conn, addr = server.accept()
-            helper.print_with_prefix('server', 'accepted connection from: {0}'.format(addr))
-            with conn:
-                conn.settimeout(timeout)
+    bufsize = 4096
+
+    def __init__(self, local_host, local_port, remote_host, remote_port, timeout):
+        self.local_host = local_host
+        self.local_port = local_port
+        self.remote_host = remote_host
+        self.remote_port = remote_port
+        self.timeout = timeout
+
+    def start(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+            server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            server.bind((self.local_host, self.local_port))
+            server.listen(1)
+            helper.print_with_prefix('server', 'listening on {0:s}:{1:d}'.format(self.local_host, self.local_port))
+            while True:
+                helper.print_with_prefix('server', 'waiting for connection')
+                conn, addr = server.accept()
+                helper.print_with_prefix('server', 'accepted connection from: {0}'.format(addr))
+                with conn:
+                    conn.settimeout(self.timeout)
+                    try:
+                        self.handle_tcp_connection(conn)
+                    except OSError as msg:
+                        helper.print_with_prefix('server', 'error occured while handling connection: {0}'.format(msg))
+
+    def handle_tcp_connection(self, conn):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as remote:
+            remote.settimeout(self.timeout)
+            remote.connect((self.remote_host, self.remote_port))
+            while True:
+                helper.print_with_prefix('connection', 'receive data from client')
+                received = False
                 try:
-                    handle_tcp_connection(conn, remote_host, remote_port, timeout)
+                    data = conn.recv(self.bufsize)
+                    if not data:
+                        helper.print_with_prefix('connection', 'no data received from client, closing')
+                        break
+                    else:
+                        received = True
                 except OSError as msg:
-                    helper.print_with_prefix('server', 'error occured while handling connection: {0}'.format(msg))
+                    helper.print_with_prefix('connection', 'error occured while receiving data from client: {0}'.format(msg))
 
-def handle_tcp_connection(conn, remote_host, remote_port, timeout):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as remote:
-        remote.settimeout(timeout)
-        remote.connect((remote_host, remote_port))
-        while True:
-            helper.print_with_prefix('connection', 'receive data from client')
-            received = False
-            try:
-                data = conn.recv(bufsize)
-                if not data:
-                    helper.print_with_prefix('connection', 'no data received from client, closing')
-                    break
-                else:
-                    received = True
-            except OSError as msg:
-                helper.print_with_prefix('connection', 'error occured while receiving data from client: {0}'.format(msg))
+                if received:
+                    helper.print_with_prefix('connection', 'received {0:d} bytes from client'.format(len(data)))
+                    # TODO: call fuzzer with client data
+                    helper.print_with_prefix('connection', 'send data to server')
+                    remote.sendall(data)
+                    helper.print_with_prefix('connection', 'sent {0:d} bytes to server'.format(len(data)))
 
-            if received:
-                helper.print_with_prefix('connection', 'received {0:d} bytes from client'.format(len(data)))
-                # TODO: call fuzzer with client data
-                helper.print_with_prefix('connection', 'send data to server')
-                remote.sendall(data)
-                helper.print_with_prefix('connection', 'sent {0:d} bytes to server'.format(len(data)))
+                helper.print_with_prefix('connection', 'receive data from server')
+                received = False
+                try:
+                    data = remote.recv(self.bufsize)
+                    if not data:
+                        helper.print_with_prefix('connection', 'no data received from server, closing')
+                        break
+                    else:
+                        received = True
+                except OSError as msg:
+                    helper.print_with_prefix('connection', 'error occured while receiving data from server: {0}'.format(msg))
 
-            helper.print_with_prefix('connection', 'receive data from server')
-            received = False
-            try:
-                data = remote.recv(bufsize)
-                if not data:
-                    helper.print_with_prefix('connection', 'no data received from server, closing')
-                    break
-                else:
-                    received = True
-            except OSError as msg:
-                helper.print_with_prefix('connection', 'error occured while receiving data from server: {0}'.format(msg))
+                if received:
+                    helper.print_with_prefix('connection', 'received {0:d} bytes from server'.format(len(data)))
+                    # TODO: call fuzzer with server data
+                    helper.print_with_prefix('connection', 'sent {0:d} bytes to client'.format(len(data)))
+                    conn.sendall(data)
 
-            if received:
-                helper.print_with_prefix('connection', 'received {0:d} bytes from server'.format(len(data)))
-                # TODO: call fuzzer with server data
-                helper.print_with_prefix('connection', 'sent {0:d} bytes to client'.format(len(data)))
-                conn.sendall(data)
-
-    helper.print_with_prefix('connection', 'closed')
-
-def fuzz(data):
-    # do nothing so far
-    return data
+        helper.print_with_prefix('connection', 'closed')
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--local_host',  help='local host name', default='localhost')
@@ -99,8 +104,9 @@ config.readargs(parser.parse_args())
 helper.config = config
 
 if config.protocol == 'tcp':
-    run_tcp_server(config.local_host, config.local_port,
-                   config.remote_host, config.remote_port,
-                   config.timeout)
+    server = Server(config.local_host, config.local_port,
+                    config.remote_host, config.remote_port,
+                    config.timeout)
+    server.start()
 elif config.protocol == 'udp':
     raise Exception('UDP is not supported yet')
