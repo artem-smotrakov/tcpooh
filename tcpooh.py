72c2563e8d11571175188c49d24337aa0cc65fd6
Artem Smotrakov <artem.smotrakov@gmail.com>
2016-11-06 19:56:23 -0800

    Better logging

--- tcpooh.py
+++ tcpooh.py
@@ -2,42 +2,89 @@
 
 import socket
 import argparse
+import textwrap
 
 # TODO: UDP support
 
-# constants
+# global variables
 bufsize = 4096
+verbose = False
+
+def verbose(*args):
+    if verbose:
+        if len(args) == 0:
+            return
+        elif len(args) == 1:
+            print(args[0])
+        elif len(args) == 2:
+            verbose_with_prefix(args[0], args[1])
+        else:
+            verbose_with_indent(args[0], args[1], args[2:])
+
+def verbose_with_prefix(prefix, message):
+    if verbose:
+        print_with_prefix(prefix, message)
+
+def verbose_with_indent(prefix, first_message, other_messages):
+    if verbose:
+        print_with_indent(prefix, first_message, other_messages)
+
+def print_with_prefix(prefix, message):
+    print('[{0:s}] {1}'.format(prefix, message))
+
+def print_with_indent(prefix, first_message, other_messages):
+    formatted_prefix = '[{0:s}] '.format(prefix)
+    print('{0:s}{1}'.format(formatted_prefix, first_message))
+    if len(other_messages) > 0:
+        indent = ' ' * len(formatted_prefix)
+        wrapper = textwrap.TextWrapper(
+            initial_indent=indent, subsequent_indent=indent, width=70)
+        for message in other_messages:
+            print(wrapper.fill(message))
 
 def run_tcp_server(local_host, local_port, remote_host, remote_port, timeout):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
         server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         server.bind((local_host, local_port))
         server.listen(1)
-        print('Listen on {0:s}:{1:d}'.format(local_host, local_port))
+        print_with_prefix('server', 'listening on {0:s}:{1:d}'.format(local_host, local_port))
         while True:
-            print('Waiting for connection')
+            print_with_prefix('server', 'waiting for connection')
             conn, addr = server.accept()
-            print('Accepted connection from', addr)
+            print_with_prefix('server', 'accepted connection from: {0}'.format(addr))
             with conn:
                 conn.settimeout(timeout)
                 try:
                     handle_tcp_connection(conn, remote_host, remote_port, timeout)
                 except OSError as msg:
-                    print('Error occured while handling connection: {0}'.format(msg))
+                    print_with_prefix('server', 'error occured while handling connection: {0}'.format(msg))
 
 def handle_tcp_connection(conn, remote_host, remote_port, timeout):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as remote:
         remote.settimeout(timeout)
         remote.connect((remote_host, remote_port))
         while True:
+            print_with_prefix('connection', 'receive data from client')
             data = conn.recv(bufsize)
             if not data:
+                print_with_prefix('connection', 'no data received from client, closing')
                 break
+            print_with_prefix('connection', 'received {0:d} bytes from client'.format(len(data)))
+            # TODO: call fuzzer with client data
+            print_with_prefix('connection', 'send data to server')
             remote.sendall(data)
+            print_with_prefix('connection', 'sent {0:d} bytes to server'.format(len(data)))
+            print_with_prefix('connection', 'receive data from server')
             data = remote.recv(bufsize)
             if not data:
+                print_with_prefix('connection', 'no data received from server, closing')
                 break
+            print_with_prefix('connection', 'received {0:d} bytes from server'.format(len(data)))
+            # TODO: call fuzzer with server data
+            print_with_prefix('connection', 'sent {0:d} bytes to client'.format(len(data)))
             conn.sendall(data)
+
+    print_with_prefix('connection', 'closed')
 
 def fuzz(data):
     # do nothing so far
@@ -55,8 +102,11 @@ parser.add_argument('--ratio',
                     default='0.01:0.05')
 parser.add_argument('--protocol', help='TCP or UDP', choices=['tcp', 'udp'], default='tcp')
 parser.add_argument('--timeout', help='Connection timeout', type=int, default=60)
+parser.add_argument('--verbose', help='more logs', action='store_true', default=False)
 
 args = parser.parse_args()
+
+verbose = args.verbose
 
 if args.test:
     parts = args.test.split(':')
